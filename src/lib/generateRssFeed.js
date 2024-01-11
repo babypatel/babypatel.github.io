30dcd9c34dc996828d0d4a4d1cef80d9c5ef7ee2
babypatel <80861089+babypatel@users.noreply.github.com>
2024-01-11 03:41:40 -0600

    Update generateRssFeed.js
    changing name pls dont break will make me sad

--- src/lib/generateRssFeed.js
+++ src/lib/generateRssFeed.js
@@ -8,8 +8,8 @@ export async function generateRssFeed() {
   let articles = await getAllArticles()
   let siteUrl = process.env.NEXT_PUBLIC_SITE_URL
   let author = {
-    name: 'Victoria Jordan',
-    email: 'vjordan.cs@gmail.com',
+    name: 'Saj Patel',
+    email: 'spate709@illinois.edu',
   }
 
   let feed = new Feed({
