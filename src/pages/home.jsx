bb61fa4b9a93a31265b24479ca93c52706e5ba8f
babypatel <80861089+babypatel@users.noreply.github.com>
2024-01-11 23:39:38 -0600

    Update home.jsx
    I changed the .jpg to png in hope of the files working now once I update it from desktop

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -12,11 +12,11 @@ import {
   GitHubIcon,
   LinkedInIcon,
 } from '@/components/SocialIcons'
-import image1 from '@/images/photos/image-1.jpg'
-import image2 from '@/images/photos/image-2.jpg'
-import image3 from '@/images/photos/image-3.jpg'
-import image4 from '@/images/photos/image-4.jpg'
-import image5 from '@/images/photos/image-5.jpg'
+import image1 from '@/images/photos/image-1.png'
+import image2 from '@/images/photos/image-2.png'
+import image3 from '@/images/photos/image-3.png'
+import image4 from '@/images/photos/image-4.png'
+import image5 from '@/images/photos/image-5.png'
 import logoDell from '@/images/logos/dell.svg'
 import logoFYPM from '@/images/logos/fypm1.svg'
 import logoGWC from '@/images/logos/gwcLogo1.png'
